57ae250cdc78a7588feffe7be066ff5b585a60aa
Guilherme Valota <gui.valota@gmail.com>
2024-12-10 20:14:47 -0300

    Versão final projeto

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -24,73 +24,71 @@ Partial Class Form1
     Private Sub InitializeComponent()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
-        Me.Label3 = New System.Windows.Forms.Label()
         Me.txtValorEmprestimo = New System.Windows.Forms.TextBox()
-        Me.txtJuros = New System.Windows.Forms.TextBox()
-        Me.txtPrazo = New System.Windows.Forms.TextBox()
+        Me.txtTaxaJuros = New System.Windows.Forms.TextBox()
         Me.btnCalcular = New System.Windows.Forms.Button()
         Me.btnLimpar = New System.Windows.Forms.Button()
-        Me.lblIofFixo = New System.Windows.Forms.Label()
-        Me.lblIofDiario = New System.Windows.Forms.Label()
-        Me.lblJuros = New System.Windows.Forms.Label()
-        Me.txtValorRealNecessiario = New System.Windows.Forms.TextBox()
+        Me.lblBruto12 = New System.Windows.Forms.Label()
+        Me.gb12 = New System.Windows.Forms.GroupBox()
+        Me.lblValorParcela12 = New System.Windows.Forms.Label()
+        Me.lblIoFDiario12 = New System.Windows.Forms.Label()
+        Me.gb24 = New System.Windows.Forms.GroupBox()
+        Me.lblValorParcela24 = New System.Windows.Forms.Label()
+        Me.lblIoFDiario24 = New System.Windows.Forms.Label()
+        Me.gb36 = New System.Windows.Forms.GroupBox()
+        Me.lblValorParcela36 = New System.Windows.Forms.Label()
+        Me.lblIoFDiario36 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.txtIof = New System.Windows.Forms.TextBox()
+        Me.lblBruto24 = New System.Windows.Forms.Label()
+        Me.lblBruto36 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
         Me.Label4 = New System.Windows.Forms.Label()
-        Me.ckbIof = New System.Windows.Forms.CheckBox()
-        Me.lblIofFixoReal = New System.Windows.Forms.Label()
-        Me.lblIofDiarioReal = New System.Windows.Forms.Label()
-        Me.lblJurosReal = New System.Windows.Forms.Label()
+        Me.txtTarifaIoFFixa = New System.Windows.Forms.TextBox()
+        Me.txtTarifaIofDiaria = New System.Windows.Forms.TextBox()
+        Me.lblJuros12 = New System.Windows.Forms.Label()
+        Me.lblJuros24 = New System.Windows.Forms.Label()
+        Me.lblJuros36 = New System.Windows.Forms.Label()
+        Me.gb12.SuspendLayout()
+        Me.gb24.SuspendLayout()
+        Me.gb36.SuspendLayout()
         Me.SuspendLayout()
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(22, 39)
+        Me.Label1.Location = New System.Drawing.Point(12, 19)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(141, 13)
+        Me.Label1.Size = New System.Drawing.Size(139, 13)
         Me.Label1.TabIndex = 0
-        Me.Label1.Text = "Valor do Emprestimo pedido:"
+        Me.Label1.Text = "Valor do Emprestimo liquido:"
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(26, 78)
+        Me.Label2.Location = New System.Drawing.Point(12, 45)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(102, 13)
         Me.Label2.TabIndex = 1
         Me.Label2.Text = "Taxa de Juros em %"
         '
-        'Label3
-        '
-        Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(26, 117)
-        Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(78, 13)
-        Me.Label3.TabIndex = 2
-        Me.Label3.Text = "Prazo em Dias:"
-        '
         'txtValorEmprestimo
         '
-        Me.txtValorEmprestimo.Location = New System.Drawing.Point(174, 36)
+        Me.txtValorEmprestimo.Location = New System.Drawing.Point(164, 16)
         Me.txtValorEmprestimo.Name = "txtValorEmprestimo"
         Me.txtValorEmprestimo.Size = New System.Drawing.Size(100, 20)
         Me.txtValorEmprestimo.TabIndex = 3
         Me.txtValorEmprestimo.Text = "80000"
+        Me.txtValorEmprestimo.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
         '
-        'txtJuros
+        'txtTaxaJuros
         '
-        Me.txtJuros.Location = New System.Drawing.Point(174, 75)
-        Me.txtJuros.Name = "txtJuros"
-        Me.txtJuros.Size = New System.Drawing.Size(100, 20)
-        Me.txtJuros.TabIndex = 4
-        Me.txtJuros.Text = "1,35"
-        '
-        'txtPrazo
-        '
-        Me.txtPrazo.Location = New System.Drawing.Point(174, 114)
-        Me.txtPrazo.Name = "txtPrazo"
-        Me.txtPrazo.Size = New System.Drawing.Size(100, 20)
-        Me.txtPrazo.TabIndex = 5
-        Me.txtPrazo.Text = "365"
+        Me.txtTaxaJuros.Location = New System.Drawing.Point(164, 42)
+        Me.txtTaxaJuros.Name = "txtTaxaJuros"
+        Me.txtTaxaJuros.Size = New System.Drawing.Size(100, 20)
+        Me.txtTaxaJuros.TabIndex = 4
+        Me.txtTaxaJuros.Text = "1,35"
+        Me.txtTaxaJuros.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
         '
         'btnCalcular
         '
@@ -110,106 +108,228 @@ Partial Class Form1
         Me.btnLimpar.Text = "Limpar"
         Me.btnLimpar.UseVisualStyleBackColor = True
         '
-        'lblIofFixo
+        'lblBruto12
         '
-        Me.lblIofFixo.AutoSize = True
-        Me.lblIofFixo.Location = New System.Drawing.Point(22, 187)
-        Me.lblIofFixo.Name = "lblIofFixo"
-        Me.lblIofFixo.Size = New System.Drawing.Size(10, 13)
-        Me.lblIofFixo.TabIndex = 9
-        Me.lblIofFixo.Text = "-"
+        Me.lblBruto12.AutoSize = True
+        Me.lblBruto12.Location = New System.Drawing.Point(6, 29)
+        Me.lblBruto12.Name = "lblBruto12"
+        Me.lblBruto12.Size = New System.Drawing.Size(54, 13)
+        Me.lblBruto12.TabIndex = 14
+        Me.lblBruto12.Text = "lblBruto12"
         '
-        'lblIofDiario
+        'gb12
         '
-        Me.lblIofDiario.AutoSize = True
-        Me.lblIofDiario.Location = New System.Drawing.Point(22, 212)
-        Me.lblIofDiario.Name = "lblIofDiario"
-        Me.lblIofDiario.Size = New System.Drawing.Size(10, 13)
-        Me.lblIofDiario.TabIndex = 10
-        Me.lblIofDiario.Text = "-"
+        Me.gb12.Controls.Add(Me.lblJuros12)
+        Me.gb12.Controls.Add(Me.lblValorParcela12)
+        Me.gb12.Controls.Add(Me.lblIoFDiario12)
+        Me.gb12.Controls.Add(Me.lblBruto12)
+        Me.gb12.Location = New System.Drawing.Point(10, 179)
+        Me.gb12.Name = "gb12"
+        Me.gb12.Size = New System.Drawing.Size(153, 186)
+        Me.gb12.TabIndex = 16
+        Me.gb12.TabStop = False
+        Me.gb12.Text = "12x"
+        Me.gb12.Visible = False
         '
-        'lblJuros
+        'lblValorParcela12
         '
-        Me.lblJuros.AutoSize = True
-        Me.lblJuros.Location = New System.Drawing.Point(22, 236)
-        Me.lblJuros.Name = "lblJuros"
-        Me.lblJuros.Size = New System.Drawing.Size(10, 13)
-        Me.lblJuros.TabIndex = 11
-        Me.lblJuros.Text = "-"
+        Me.lblValorParcela12.AutoSize = True
+        Me.lblValorParcela12.Location = New System.Drawing.Point(1, 152)
+        Me.lblValorParcela12.Name = "lblValorParcela12"
+        Me.lblValorParcela12.Size = New System.Drawing.Size(89, 13)
+        Me.lblValorParcela12.TabIndex = 3
+        Me.lblValorParcela12.Text = "lblValorParcela12"
         '
-        'txtValorRealNecessiario
+        'lblIoFDiario12
         '
-        Me.txtValorRealNecessiario.Location = New System.Drawing.Point(174, 278)
-        Me.txtValorRealNecessiario.Name = "txtValorRealNecessiario"
-        Me.txtValorRealNecessiario.Size = New System.Drawing.Size(100, 20)
-        Me.txtValorRealNecessiario.TabIndex = 13
+        Me.lblIoFDiario12.AutoSize = True
+        Me.lblIoFDiario12.Location = New System.Drawing.Point(6, 94)
+        Me.lblIoFDiario12.Name = "lblIoFDiario12"
+        Me.lblIoFDiario12.Size = New System.Drawing.Size(71, 13)
+        Me.lblIoFDiario12.TabIndex = 1
+        Me.lblIoFDiario12.Text = "lblIoFDiario12"
+        '
+        'gb24
+        '
+        Me.gb24.Controls.Add(Me.lblJuros24)
+        Me.gb24.Controls.Add(Me.lblBruto24)
+        Me.gb24.Controls.Add(Me.lblValorParcela24)
+        Me.gb24.Controls.Add(Me.lblIoFDiario24)
+        Me.gb24.Location = New System.Drawing.Point(174, 179)
+        Me.gb24.Name = "gb24"
+        Me.gb24.Size = New System.Drawing.Size(153, 186)
+        Me.gb24.TabIndex = 17
+        Me.gb24.TabStop = False
+        Me.gb24.Text = "24x"
+        Me.gb24.Visible = False
+        '
+        'lblValorParcela24
+        '
+        Me.lblValorParcela24.AutoSize = True
+        Me.lblValorParcela24.Location = New System.Drawing.Point(13, 152)
+        Me.lblValorParcela24.Name = "lblValorParcela24"
+        Me.lblValorParcela24.Size = New System.Drawing.Size(89, 13)
+        Me.lblValorParcela24.TabIndex = 4
+        Me.lblValorParcela24.Text = "lblValorParcela24"
+        '
+        'lblIoFDiario24
+        '
+        Me.lblIoFDiario24.AutoSize = True
+        Me.lblIoFDiario24.Location = New System.Drawing.Point(13, 94)
+        Me.lblIoFDiario24.Name = "lblIoFDiario24"
+        Me.lblIoFDiario24.Size = New System.Drawing.Size(71, 13)
+        Me.lblIoFDiario24.TabIndex = 2
+        Me.lblIoFDiario24.Text = "lblIoFDiario24"
+        '
+        'gb36
+        '
+        Me.gb36.Controls.Add(Me.lblJuros36)
+        Me.gb36.Controls.Add(Me.lblBruto36)
+        Me.gb36.Controls.Add(Me.lblValorParcela36)
+        Me.gb36.Controls.Add(Me.lblIoFDiario36)
+        Me.gb36.Location = New System.Drawing.Point(333, 179)
+        Me.gb36.Name = "gb36"
+        Me.gb36.Size = New System.Drawing.Size(153, 186)
+        Me.gb36.TabIndex = 17
+        Me.gb36.TabStop = False
+        Me.gb36.Text = "36x"
+        Me.gb36.Visible = False
+        '
+        'lblValorParcela36
+        '
+        Me.lblValorParcela36.AutoSize = True
+        Me.lblValorParcela36.Location = New System.Drawing.Point(6, 152)
+        Me.lblValorParcela36.Name = "lblValorParcela36"
+        Me.lblValorParcela36.Size = New System.Drawing.Size(89, 13)
+        Me.lblValorParcela36.TabIndex = 5
+        Me.lblValorParcela36.Text = "lblValorParcela36"
+        '
+        'lblIoFDiario36
+        '
+        Me.lblIoFDiario36.AutoSize = True
+        Me.lblIoFDiario36.Location = New System.Drawing.Point(6, 94)
+        Me.lblIoFDiario36.Name = "lblIoFDiario36"
+        Me.lblIoFDiario36.Size = New System.Drawing.Size(71, 13)
+        Me.lblIoFDiario36.TabIndex = 3
+        Me.lblIoFDiario36.Text = "lblIoFDiario36"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(12, 71)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(25, 13)
+        Me.Label6.TabIndex = 18
+        Me.Label6.Text = "IoF:"
+        '
+        'txtIof
+        '
+        Me.txtIof.Location = New System.Drawing.Point(164, 68)
+        Me.txtIof.Name = "txtIof"
+        Me.txtIof.ReadOnly = True
+        Me.txtIof.Size = New System.Drawing.Size(99, 20)
+        Me.txtIof.TabIndex = 19
+        Me.txtIof.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'lblBruto24
+        '
+        Me.lblBruto24.AutoSize = True
+        Me.lblBruto24.Location = New System.Drawing.Point(13, 29)
+        Me.lblBruto24.Name = "lblBruto24"
+        Me.lblBruto24.Size = New System.Drawing.Size(54, 13)
+        Me.lblBruto24.TabIndex = 5
+        Me.lblBruto24.Text = "lblBruto24"
+        '
+        'lblBruto36
+        '
+        Me.lblBruto36.AutoSize = True
+        Me.lblBruto36.Location = New System.Drawing.Point(6, 29)
+        Me.lblBruto36.Name = "lblBruto36"
+        Me.lblBruto36.Size = New System.Drawing.Size(54, 13)
+        Me.lblBruto36.TabIndex = 6
+        Me.lblBruto36.Text = "lblBruto36"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(288, 19)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(74, 13)
+        Me.Label3.TabIndex = 20
+        Me.Label3.Text = "Taria IoF Fixa:"
         '
         'Label4
         '
         Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(17, 281)
+        Me.Label4.Location = New System.Drawing.Point(288, 45)
         Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(146, 13)
-        Me.Label4.TabIndex = 14
-        Me.Label4.Text = "Valor emprestimo Necessário:"
+        Me.Label4.Size = New System.Drawing.Size(85, 13)
+        Me.Label4.TabIndex = 21
+        Me.Label4.Text = "Tarifa IoF Diária:"
         '
-        'ckbIof
+        'txtTarifaIoFFixa
         '
-        Me.ckbIof.AutoSize = True
-        Me.ckbIof.Location = New System.Drawing.Point(25, 12)
-        Me.ckbIof.Name = "ckbIof"
-        Me.ckbIof.Size = New System.Drawing.Size(119, 17)
-        Me.ckbIof.TabIndex = 15
-        Me.ckbIof.Text = "IOF no emprestimos"
-        Me.ckbIof.UseVisualStyleBackColor = True
+        Me.txtTarifaIoFFixa.Location = New System.Drawing.Point(386, 16)
+        Me.txtTarifaIoFFixa.Name = "txtTarifaIoFFixa"
+        Me.txtTarifaIoFFixa.Size = New System.Drawing.Size(100, 20)
+        Me.txtTarifaIoFFixa.TabIndex = 22
+        Me.txtTarifaIoFFixa.Text = "0.0038"
+        Me.txtTarifaIoFFixa.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
         '
-        'lblIofFixoReal
+        'txtTarifaIofDiaria
         '
-        Me.lblIofFixoReal.AutoSize = True
-        Me.lblIofFixoReal.Location = New System.Drawing.Point(270, 187)
-        Me.lblIofFixoReal.Name = "lblIofFixoReal"
-        Me.lblIofFixoReal.Size = New System.Drawing.Size(10, 13)
-        Me.lblIofFixoReal.TabIndex = 16
-        Me.lblIofFixoReal.Text = "-"
+        Me.txtTarifaIofDiaria.Location = New System.Drawing.Point(386, 45)
+        Me.txtTarifaIofDiaria.Name = "txtTarifaIofDiaria"
+        Me.txtTarifaIofDiaria.Size = New System.Drawing.Size(100, 20)
+        Me.txtTarifaIofDiaria.TabIndex = 23
+        Me.txtTarifaIofDiaria.Text = "0.000082"
+        Me.txtTarifaIofDiaria.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
         '
-        'lblIofDiarioReal
+        'lblJuros12
         '
-        Me.lblIofDiarioReal.AutoSize = True
-        Me.lblIofDiarioReal.Location = New System.Drawing.Point(270, 212)
-        Me.lblIofDiarioReal.Name = "lblIofDiarioReal"
-        Me.lblIofDiarioReal.Size = New System.Drawing.Size(10, 13)
-        Me.lblIofDiarioReal.TabIndex = 17
-        Me.lblIofDiarioReal.Text = "-"
+        Me.lblJuros12.AutoSize = True
+        Me.lblJuros12.Location = New System.Drawing.Point(6, 61)
+        Me.lblJuros12.Name = "lblJuros12"
+        Me.lblJuros12.Size = New System.Drawing.Size(54, 13)
+        Me.lblJuros12.TabIndex = 15
+        Me.lblJuros12.Text = "lblJuros12"
         '
-        'lblJurosReal
+        'lblJuros24
         '
-        Me.lblJurosReal.AutoSize = True
-        Me.lblJurosReal.Location = New System.Drawing.Point(270, 236)
-        Me.lblJurosReal.Name = "lblJurosReal"
-        Me.lblJurosReal.Size = New System.Drawing.Size(10, 13)
-        Me.lblJurosReal.TabIndex = 18
-        Me.lblJurosReal.Text = "-"
+        Me.lblJuros24.AutoSize = True
+        Me.lblJuros24.Location = New System.Drawing.Point(13, 61)
+        Me.lblJuros24.Name = "lblJuros24"
+        Me.lblJuros24.Size = New System.Drawing.Size(54, 13)
+        Me.lblJuros24.TabIndex = 6
+        Me.lblJuros24.Text = "lblJuros24"
+        '
+        'lblJuros36
+        '
+        Me.lblJuros36.AutoSize = True
+        Me.lblJuros36.Location = New System.Drawing.Point(6, 61)
+        Me.lblJuros36.Name = "lblJuros36"
+        Me.lblJuros36.Size = New System.Drawing.Size(54, 13)
+        Me.lblJuros36.TabIndex = 7
+        Me.lblJuros36.Text = "lblJuros36"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(467, 310)
-        Me.Controls.Add(Me.lblJurosReal)
-        Me.Controls.Add(Me.lblIofDiarioReal)
-        Me.Controls.Add(Me.lblIofFixoReal)
-        Me.Controls.Add(Me.ckbIof)
+        Me.ClientSize = New System.Drawing.Size(503, 377)
+        Me.Controls.Add(Me.txtTarifaIofDiaria)
+        Me.Controls.Add(Me.txtTarifaIoFFixa)
         Me.Controls.Add(Me.Label4)
-        Me.Controls.Add(Me.txtValorRealNecessiario)
-        Me.Controls.Add(Me.lblJuros)
-        Me.Controls.Add(Me.lblIofDiario)
-        Me.Controls.Add(Me.lblIofFixo)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtIof)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.gb36)
+        Me.Controls.Add(Me.gb24)
+        Me.Controls.Add(Me.gb12)
         Me.Controls.Add(Me.btnLimpar)
         Me.Controls.Add(Me.btnCalcular)
-        Me.Controls.Add(Me.txtPrazo)
-        Me.Controls.Add(Me.txtJuros)
+        Me.Controls.Add(Me.txtTaxaJuros)
         Me.Controls.Add(Me.txtValorEmprestimo)
-        Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
@@ -218,6 +338,12 @@ Partial Class Form1
         Me.Name = "Form1"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Teste de Simulador"
+        Me.gb12.ResumeLayout(False)
+        Me.gb12.PerformLayout()
+        Me.gb24.ResumeLayout(False)
+        Me.gb24.PerformLayout()
+        Me.gb36.ResumeLayout(False)
+        Me.gb36.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -225,19 +351,29 @@ Partial Class Form1
 
     Friend WithEvents Label1 As Label
     Friend WithEvents Label2 As Label
-    Friend WithEvents Label3 As Label
     Friend WithEvents txtValorEmprestimo As TextBox
-    Friend WithEvents txtJuros As TextBox
-    Friend WithEvents txtPrazo As TextBox
+    Friend WithEvents txtTaxaJuros As TextBox
     Friend WithEvents btnCalcular As Button
     Friend WithEvents btnLimpar As Button
-    Friend WithEvents lblIofFixo As Label
-    Friend WithEvents lblIofDiario As Label
-    Friend WithEvents lblJuros As Label
-    Friend WithEvents txtValorRealNecessiario As TextBox
+    Friend WithEvents lblBruto12 As Label
+    Friend WithEvents gb12 As GroupBox
+    Friend WithEvents lblIoFDiario12 As Label
+    Friend WithEvents gb24 As GroupBox
+    Friend WithEvents gb36 As GroupBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents lblValorParcela12 As Label
+    Friend WithEvents lblValorParcela24 As Label
+    Friend WithEvents lblIoFDiario24 As Label
+    Friend WithEvents lblValorParcela36 As Label
+    Friend WithEvents lblIoFDiario36 As Label
+    Friend WithEvents txtIof As TextBox
+    Friend WithEvents lblBruto24 As Label
+    Friend WithEvents lblBruto36 As Label
+    Friend WithEvents Label3 As Label
     Friend WithEvents Label4 As Label
-    Friend WithEvents ckbIof As CheckBox
-    Friend WithEvents lblIofFixoReal As Label
-    Friend WithEvents lblIofDiarioReal As Label
-    Friend WithEvents lblJurosReal As Label
+    Friend WithEvents txtTarifaIoFFixa As TextBox
+    Friend WithEvents txtTarifaIofDiaria As TextBox
+    Friend WithEvents lblJuros12 As Label
+    Friend WithEvents lblJuros24 As Label
+    Friend WithEvents lblJuros36 As Label
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,53 +1,49 @@
 ﻿Public Class Form1
-    Dim contadorRecontagem = 2000
-    Dim taxaTolerancia As Decimal = 0.001 '10%
-    Dim valorInicialRequisitado As Decimal
-    Dim valorNecessario As Decimal
-    Dim taxaJuros As Decimal = 1
-    Dim prazo As Integer
+    Dim tarifaIofFixa As Decimal = 0.0038
+    Dim tarifaIofDiaria As Decimal = 0.000082
     Private Sub btnLimpar_Click(sender As Object, e As EventArgs) Handles btnLimpar.Click
         txtValorEmprestimo.Text = ""
-        txtJuros.Text = ""
-        txtPrazo.Text = ""
-        lblIofFixo.Text = ""
-        lblJuros.Text = ""
-        lblIofDiario.Text = ""
-        txtValorRealNecessiario.Text = ""
-        taxaTolerancia = 0
+        txtTaxaJuros.Text = ""
+        txtTarifaIoFFixa.Text = 0.0038
+        txtTarifaIofDiaria.Text = 0.000082
+        txtIof.Text = ""
+        gb12.Visible = False
+        gb24.Visible = False
+        gb36.Visible = False
     End Sub
 
     Private Sub btnCalcular_Click(sender As Object, e As EventArgs) Handles btnCalcular.Click
         Try
             Cursor = Cursors.WaitCursor
-
-            Dim valorEmprestimo As Decimal = Decimal.Parse(txtValorEmprestimo.Text)
-            taxaJuros = Decimal.Parse(txtJuros.Text) / 100
-            prazo = Integer.Parse(txtPrazo.Text)
-
-            Dim iofFixo = valorEmprestimo * 0.0038
-            Dim iofDiario = valorEmprestimo * 0.000082 * prazo
-            Dim juros = valorEmprestimo * taxaJuros
-            valorInicialRequisitado = valorEmprestimo
-            Dim valorTotal As Decimal = 0
-            If ckbIof.Checked Then
-                Dim NvalorEmprestimo = valorEmprestimo + iofFixo + iofDiario
-                valorNecessario = NvalorEmprestimo
-                Recalcular(NvalorEmprestimo)
-                NvalorEmprestimo = Decimal.Parse(txtValorRealNecessiario.Text)
-                Dim iofFixoReal = NvalorEmprestimo * 0.0038
-                Dim iofDiarioReal = NvalorEmprestimo * 0.000082 * prazo
-                Dim jurosReal = NvalorEmprestimo * taxaJuros
-                lblIofFixoReal.Text = "IOF Fixo Real: " & iofFixoReal.ToString("N2")
-                lblIofDiarioReal.Text = "IOF Diário Real: " & iofDiarioReal.ToString("N2")
-                lblJurosReal.Text = "Juros Real: " & jurosReal.ToString("N2")
-            Else
-                valorTotal = Decimal.Parse(txtValorRealNecessiario.Text) + iofFixo + iofDiario + juros
+            If Not IsNumeric(txtValorEmprestimo.Text) Or Not IsNumeric(txtTaxaJuros.Text) Or
+                Not IsNumeric(txtTarifaIoFFixa.Text) Or Not IsNumeric(txtTarifaIofDiaria.Text) Then
+                MessageBox.Show("Informe apenas números")
+                Exit Sub
             End If
-            lblIofFixo.Text = "IOF Fixo: " & iofFixo.ToString("N2")
-            lblIofDiario.Text = "IOF Diário: " & iofDiario.ToString("N2")
-            lblJuros.Text = "Juros: " & juros.ToString("N2")
-            txtValorEmprestimo.Text = valorInicialRequisitado.ToString("N2")
+            tarifaIofFixa = txtTarifaIoFFixa.Text.Replace(".", ",")
+            tarifaIofDiaria = txtTarifaIofDiaria.Text.Replace(".", ",")
+            Dim valorEmprestimo As Decimal = Decimal.Parse(txtValorEmprestimo.Text)
+            Dim taxaJuros = Decimal.Parse(txtTaxaJuros.Text) / 100
+            txtIof.Text = (valorEmprestimo * tarifaIofFixa).ToString("C")
 
+            lblBruto12.Text = CalcularValorBrutoComIof(valorEmprestimo, tarifaIofFixa, tarifaIofDiaria, 365, taxaJuros)
+            lblIoFDiario12.Text = "IoF Diário:" & (valorEmprestimo * tarifaIofDiaria * 365).ToString("C")
+            lblValorParcela12.Text = "Parcela:" & (CalcularValorBrutoComIof(valorEmprestimo, tarifaIofFixa, tarifaIofDiaria, 365, taxaJuros) / 12).ToString("C")
+            lblJuros12.Text = "Juros:" & (Decimal.Parse(valorEmprestimo) * taxaJuros).ToString("C")
+            lblBruto12.Text = "Valor Bruto:" & Decimal.Parse(lblBruto12.Text).ToString("C")
+            gb12.Visible = True
+            lblBruto24.Text = CalcularValorBrutoComIof(valorEmprestimo, tarifaIofFixa, tarifaIofDiaria, 730, taxaJuros)
+            lblIoFDiario24.Text = "IoF Diário:" & (valorEmprestimo * tarifaIofDiaria * 730).ToString("C")
+            lblValorParcela24.Text = "Parcela:" & (CalcularValorBrutoComIof(valorEmprestimo, tarifaIofFixa, tarifaIofDiaria, 730, taxaJuros) / 24).ToString("C")
+            lblJuros24.Text = "Juros:" & (Decimal.Parse(valorEmprestimo) * taxaJuros).ToString("C")
+            lblBruto24.Text = "Valor Bruto:" & Decimal.Parse(lblBruto24.Text).ToString("C")
+            gb24.Visible = True
+            lblBruto36.Text = CalcularValorBrutoComIof(valorEmprestimo, tarifaIofFixa, tarifaIofDiaria, 1095, taxaJuros)
+            lblIoFDiario36.Text = "IoF Diário:" & (valorEmprestimo * tarifaIofDiaria * 1095).ToString("C")
+            lblValorParcela36.Text = "Parcela:" & (CalcularValorBrutoComIof(valorEmprestimo, tarifaIofFixa, tarifaIofDiaria, 168, taxaJuros) / 36).ToString("C")
+            lblJuros36.Text = "Juros:" & (Decimal.Parse(valorEmprestimo) * taxaJuros).ToString("C")
+            lblBruto36.Text = "Valor Bruto:" & Decimal.Parse(lblBruto36.Text).ToString("C")
+            gb36.Visible = True
         Catch ex As Exception
             MessageBox.Show("Erro:" & ex.Message)
         Finally
@@ -55,27 +51,14 @@
         End Try
     End Sub
 
-    Private Sub Recalcular(NvalorEmprestimo As Decimal)
-        Try
-            contadorRecontagem -= 1
-            Dim valorTotal = NvalorEmprestimo + (NvalorEmprestimo * 0.0038) + (NvalorEmprestimo * 0.000082 * prazo)
-            Console.WriteLine("Contagem de loops: " & contadorRecontagem)
-            Console.WriteLine("Valor simulado:" & NvalorEmprestimo)
-            If contadorRecontagem > 0 Then
-                If Math.Round(NvalorEmprestimo, 2) > Math.Round(valorInicialRequisitado, 2) Then
-                    Recalcular(NvalorEmprestimo - 1)
-                ElseIf Math.Round(NvalorEmprestimo, 2) < Math.Round(valorInicialRequisitado, 2) Then
-                    Recalcular(NvalorEmprestimo + 1)
-                Else
-                    txtValorRealNecessiario.Text = NvalorEmprestimo.ToString("N2")
-                    Exit Sub
-                End If
-            Else
-                txtValorRealNecessiario.Text = NvalorEmprestimo.ToString("N2")
-                Exit Sub
-            End If
-        Catch ex As Exception
-            MessageBox.Show("Erro:" & ex.Message)
-        End Try
-    End Sub
+
+    Private Function CalcularValorBrutoComIof(valorLiquido As Decimal, taxaIoFFixo As Decimal, taxaIoFDiaria As Decimal, dias As Integer, taxaJuros As Decimal) As Decimal
+        Dim taxaTotal As Decimal = taxaIoFFixo + (taxaIoFDiaria * dias) + taxaJuros
+        If taxaTotal >= 1 Then
+            Throw New ArgumentException("A soma das taxas de IOF não pode ser maior que 1.")
+        End If
+        Dim valorBruto As Decimal = valorLiquido / (1 - taxaTotal)
+        Return valorBruto
+    End Function
+
 End Class
